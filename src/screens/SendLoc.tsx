e411b59f21357d8b779f018c177053a0ac85d1fa
MSamiDev <mohammadsami@duck.com>
2023-07-11 07:20:27 +0530

    ADD: Add background location tracking

--- src/screens/SendLoc.tsx
+++ src/screens/SendLoc.tsx
@@ -5,16 +5,22 @@ import {
   TouchableOpacity,
   Button,
   PermissionsAndroid,
+  Platform,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../App';
 import database from '@react-native-firebase/database';
 import {firebase} from '@react-native-firebase/database';
-// import Geolocation from 'react-native-geolocation-service';
 import Geolocation, {
   GeolocationResponse,
+  GeolocationError,
 } from '@react-native-community/geolocation';
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
+
+// import Geolocation, {
+//   GeolocationResponse,
+// } from '@react-native-community/geolocation';
 
 const reference = firebase
   .app()
@@ -54,47 +60,118 @@ type SendLocProps = NativeStackScreenProps<RootStackParamList, 'SendLoc'>;
 
 const SendLoc = ({navigation}: SendLocProps) => {
   // const [location, setLocation] = useState({});
+  // const [location, setLocation] = useState<GeolocationResponse | null>(null);
+  // const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+
   const [location, setLocation] = useState<GeolocationResponse | null>(null);
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+  const [watchId, setWatchId] = useState<number | null>(null);
 
-  const startFetchingLocation = () => {
-    const id = setInterval(fetchLocation, 30000); // Fetch location every 30 seconds
-    setIntervalId(id);
-    fetchLocation(); // Fetch location immediately
-  };
-
-  const fetchLocation = () => {
-    Geolocation.getCurrentPosition(
-      position => {
+  const startTracking = () => {
+    const id = Geolocation.watchPosition(
+      (position: GeolocationResponse) => {
         setLocation(position);
         console.log(position);
+        console.log('watching');
         database()
           .ref('/locations/3')
           .set({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
-            timestamp: Date()
+            timestamp: Date(),
           })
           .then(() => console.log('Data set.'));
       },
-      error => {
+      (error: GeolocationError) => {
         console.error('Error fetching location:', error);
       },
+      {
+        enableHighAccuracy: true,
+        distanceFilter: 1,
+        interval: 5000, // Update interval in milliseconds (e.g., 5000 for every 5 seconds)
+        fastestInterval: 2000, // Fastest update interval in milliseconds
+      },
     );
+    setWatchId(id);
   };
 
-  const stopFetchingLocation = () => {
-    if (intervalId) {
-      clearInterval(intervalId);
-      setIntervalId(null);
-    }
-  };
+  // useEffect(() => {
+  //   return () => {
+  //     stopTracking(); // Stop observing location updates when the component unmounts
+  //   };
+  // }, []);
+  // ------------------------------------------------
+  // const [location, setLocation] = useState<Location | null>(null);
 
-  useEffect(() => {
-    if (intervalId) {
-      fetchLocation(); // Fetch location immediately on app start
-    }
-  }, [intervalId]);
+  // const startTracking = () => {
+  //   BackgroundGeolocation.start(); // Start tracking location in the background
+
+  //   BackgroundGeolocation.onLocation((location: Location) => {
+  //     setLocation(location);
+  //   });
+
+  //   BackgroundGeolocation.onLocationError((error: Error) => {
+  //     console.error('Error fetching location:', error);
+  //   });
+  // };
+
+  // const stopTracking = () => {
+  //   BackgroundGeolocation.stop(); // Stop tracking location in the background
+  // };
+
+  // useEffect(() => {
+  //   BackgroundGeolocation.checkStatus((status: any) => {
+  //     if (!status.isRunning) {
+  //       startTracking();
+  //     }
+  //   });
+
+  //   return () => {
+  //     BackgroundGeolocation.removeAllListeners(); // Clean up event listeners when component unmounts
+  //   };
+  // }, []);
+
+  // ------------------------------------------------
+
+  // const startFetchingLocation = () => {
+  //   const id = setInterval(fetchLocation, 30000); // Fetch location every 30 seconds
+  //   setIntervalId(id);
+  //   fetchLocation(); // Fetch location immediately
+  // };
+
+  // const fetchLocation = () => {
+  //   Geolocation.getCurrentPosition(
+  //     position => {
+  //       setLocation(position);
+  //       console.log(position);
+  //       database()
+  //         .ref('/locations/3')
+  //         .set({
+  //           latitude: position.coords.latitude,
+  //           longitude: position.coords.longitude,
+  //           timestamp: Date()
+  //         })
+  //         .then(() => console.log('Data set.'));
+  //     },
+  //     error => {
+  //       console.error('Error fetching location:', error);
+  //     },
+  //   );
+  // };
+
+  // const stopFetchingLocation = () => {
+  //   if (intervalId) {
+  //     clearInterval(intervalId);
+  //     setIntervalId(null);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (intervalId) {
+  //     fetchLocation(); // Fetch location immediately on app start
+  //   }
+  // }, [intervalId]);
+
+  // ------------------------------------------------
 
   //   const getLocation = () => {
   //     const result = requestLocationPermission();
@@ -118,13 +195,52 @@ const SendLoc = ({navigation}: SendLocProps) => {
   //     console.log(location);
   //   };
 
+  useEffect(() => {
+    ReactNativeForegroundService.add_task( () => startTracking(), {
+      delay: 1000,
+      onLoop: true,
+      taskId: 'taskid',
+      onError: e => console.log(`Error logging:`, e),
+    });
+  }, []);
+
+  const stopTracking = () => {
+    if (watchId) {
+      Geolocation.clearWatch(watchId);
+      setWatchId(null);
+    }
+    console.log('stop');
+    ReactNativeForegroundService.remove_task('taskid');
+    stopTask();
+  };
+
+  const StartTask = () => {
+    ReactNativeForegroundService.start({
+      id: 1244,
+      title: 'SafeAir Tracker',
+      message: 'Your Location is being tracked',
+      icon: 'ic_launcher',
+      button: true,
+      button2: true,
+      buttonText: 'More Info',
+      button2Text: 'Stop',
+      buttonOnPress: 'cray',
+      color: '#000000',
+    });
+  };
+
+  const stopTask = () => {
+    ReactNativeForegroundService.stop();
+  };
+
+
   return (
     <View style={styles.ButtonContainer}>
       <Text style={styles.Heading}>Send Location</Text>
-      <TouchableOpacity style={styles.button} onPress={startFetchingLocation}>
+      <TouchableOpacity style={styles.button} onPress={StartTask}>
         <Text style={styles.buttonText}>START</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={stopFetchingLocation}>
+      <TouchableOpacity style={styles.button} onPress={stopTracking}>
         <Text style={styles.buttonText}>STOP</Text>
       </TouchableOpacity>
       {location && (
